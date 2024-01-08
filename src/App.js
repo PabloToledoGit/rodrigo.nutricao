9d3d53583232f91cab177711f1164e29bc5042e3
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2024-01-07 23:05:30 -0300

    criando titulo e foto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import React from 'react';
 import './reset.css';
+import Titulo from './components/titulo/titulo.js';
 import Header from './components/header/header.js';
+import './rodrigo.svg';
+import rodrigoSvg from './rodrigo.svg';
+
+
 function App() {
   return (
     <div className="App">
-      <Header />  
+      <Header />
+      <div className='divmeio'>
+      <Titulo />
+      <img className='imagemrodrigo' src={rodrigoSvg} alt="" />
+      </div>
     </div>
   );
 }
---
+++ src/components/titulo/titulo.js
@@ -0,0 +1,12 @@
+import "./titulo.css";
+import React from "react";
+
+function Titulo() {
+    return (
+        <h1 className="titulofrase">
+            Alcance a sua <strong className="corverde">melhor versão</strong>
+        </h1>
+    );
+}
+
+export default Titulo;
